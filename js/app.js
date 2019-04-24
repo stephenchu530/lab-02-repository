65b15caf34ce87ad081c0de2ced36a7b98b31252
Levi Porter <levi@levibrooke.com>
2019-04-24 10:51:40 -0700

    Refactor animal section creation in fn, filter and add keywords to an array

--- js/app.js
+++ js/app.js
@@ -16,11 +16,34 @@ let AnimalObj = function(filePath, title, descript, keyword, horns) {
 $.get('../data/page-1.json', data => {
   data.forEach(ele => {
     new AnimalObj(ele.image_url, ele.title, ele.description, ele.keyword, ele.horns);
-    const section = $('#photo-template').clone();
-    section.children('h2').append(ele.title);
-    section.children('img').attr('src', ele.image_url);
-    section.children('img').attr('alt', ele.title);
-    section.children('p').append(ele.description);
-    $('main').append(section);
+
+    makeAnimalSection(ele);
   });
+  makeAnimalKeywords(animalArray);
 });
+
+// create clone of animal section
+let makeAnimalSection = (ele) => {
+  const section = $('#photo-template').clone();
+  section.children('h2').append(ele.title);
+  section.children('img').attr('src', ele.image_url);
+  section.children('img').attr('alt', ele.title);
+  section.children('p').append(ele.description);
+  $('main').append(section);
+};
+
+// create array of keywords
+const keywords = [];
+
+// check if already exists
+let makeAnimalKeywords = (arr) => {
+
+  arr.forEach(el => {
+    if (!keywords.includes(el.keyword)) {
+      keywords.push(el.keyword);
+    }
+  });
+  console.log('keywords', keywords);
+};
+
+
